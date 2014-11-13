b96bd5b758f1948234e9fb16a539feeff1390265
Yi Zhao <yizhao.cs@gmail.com>
2014-11-13 14:29:43 -0800

    first commit

---
+++ src/BagOfPrimitives.java
@@ -0,0 +1,9 @@
+public class BagOfPrimitives {
+	private int value1 = 1;
+	private String value2 = "abc";
+	private transient int value3 = 3;
+
+	BagOfPrimitives() {
+		// no-args constructor
+	}
+}
